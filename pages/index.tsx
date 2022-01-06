3adbec95d4ad17dfd63486150e872f19a5d50169
Vkar90 <kardamitsis.e@gmail.com>
2022-01-06 10:47:59 +0200

    set up 3 basic components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget} from '../components';
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing'},
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
+];
 
 export default function Home() {
   return (
@@ -7,7 +13,16 @@ export default function Home() {
         <title>Ok Boomer Mag</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+          <div className='lg:col-span-8 col-span-1'>
+            {posts.map((post, index) => <PostCard post={post} key= {post.title} />)}
+          </div>
+          <div className='lg:col-span-4 col-span-1'>
+              <div className='lg:sticky relative top-8'>
 
+              </div>
+          </div>
+        </div> 
      
     </div>
   )
