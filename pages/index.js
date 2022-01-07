085060483a9543fa0f17b39fadf957214e08eea0
Vkar90 <kardamitsis.e@gmail.com>
2022-01-07 16:24:48 +0200

    fetch post data from graphql and create PostCard

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,17 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget} from '../components';
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing'},
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Ok Boomer Mag</title>
         <link rel="icon" href="/favicon.ico" />
-        <script defer src="https://unpkg.com/alpinejs@3.7.1/dist/cdn.min.js"></script>
       </Head>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
           <div className='lg:col-span-8 col-span-1'>
-            {posts.map((post) => <PostCard post={post} key= {post.title} />)}
+            {posts.map((post) => <PostCard post={post.node} key= {post.title} />)}
           </div>
           <div className='lg:col-span-4 col-span-1'>
               <div className='lg:sticky relative top-8'>
@@ -27,4 +22,12 @@ export default function Home() {
         </div> 
     </div>
   )
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
